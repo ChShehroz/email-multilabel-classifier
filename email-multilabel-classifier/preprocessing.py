d4e49fb521c80d60c151c5b93a889d87ae0bdb49
Shehroz <mshehroz302@gmail.com>
2026-03-10 02:06:43 +0000

    Create preprocessing.py

---
+++ email-multilabel-classifier/preprocessing.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import config
+
+def load_data():
+    df = pd.read_csv(config.DATA_PATH)
+    return df
+
+
+def preprocess_data(df):
+    df = df.dropna()
+
+    X = df[config.TEXT_COLUMN]
+
+    y2 = df[config.TYPE2_COLUMN]
+    y3 = df[config.TYPE3_COLUMN]
+    y4 = df[config.TYPE4_COLUMN]
+
+    return X, y2, y3, y4
+
+def split_data(X, y2, y3, y4):
+
+    return train_test_split(
+        X, y2, y3, y4,
+        test_size=config.TEST_SIZE,
+        random_state=config.RANDOM_STATE
+    )
