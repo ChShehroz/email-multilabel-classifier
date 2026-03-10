89fcb70662037f3f9103bd50fa5bf4f361e64812
Shehroz <mshehroz302@gmail.com>
2026-03-10 20:54:36 +0000

    Update

--- email-multilabel-classifier/Databundle.py
+++ email-multilabel-classifier/Databundle.py
@@ -1,4 +1,4 @@
-class DataBundle:
+class Databundle:
 
     def __init__(self, X_train, X_test, y_train, y_test):
 
--- email-multilabel-classifier/main.py
+++ email-multilabel-classifier/main.py
@@ -1,7 +1,7 @@
 import preprocessing
 import feature_extraction
 import targets
-import dataBundle
+import Databundle
 import model
 import evaluator
 
@@ -21,7 +21,7 @@ def main():
     y_train_targets = targets.build_targets(y2_train, y3_train, y4_train)
     y_test_targets = targets.build_targets(y2_test, y3_test, y4_test)
 
-    bundle = dataBundle.DataBundle(
+    bundle = Databundle.DataBundle(
         X_train_vec,
         X_test_vec,
         y_train_targets,
