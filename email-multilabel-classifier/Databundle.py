85644468d98e7de5e44701eef0494c8b42d706e9
Shehroz <112334909+ChShehroz@users.noreply.github.com>
2026-03-25 20:17:48 +0000

    Update Databundle.py

--- email-multilabel-classifier/Databundle.py
+++ email-multilabel-classifier/Databundle.py
@@ -1,8 +1,9 @@
 class Databundle:
 
-    def __init__(self, X_train, X_test, y_train, y_test):
+    def __init__(self, X_train, X_test, y_train, y_test, metadata=None):
 
         self.X_train = X_train
         self.X_test = X_test
         self.y_train = y_train
         self.y_test = y_test
+        self.metadata = metadata or {}
