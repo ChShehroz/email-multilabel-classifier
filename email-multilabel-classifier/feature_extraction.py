5a536144dfa34308c171432ee9bf0be84a26b0cc
Shehroz <112334909+ChShehroz@users.noreply.github.com>
2026-03-25 20:19:30 +0000

    Update feature_extraction.py

--- email-multilabel-classifier/feature_extraction.py
+++ email-multilabel-classifier/feature_extraction.py
@@ -2,10 +2,13 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def vectorize_text(X_train, X_test):
-
-    vectorizer = TfidfVectorizer(stop_words='english')
-
+    vectorizer = TfidfVectorizer(
+        lowercase=True,
+        stop_words="english",
+        ngram_range=(1, 2),
+        min_df=1,
+        max_features=5000
+    )
     X_train_vec = vectorizer.fit_transform(X_train)
     X_test_vec = vectorizer.transform(X_test)
-
     return X_train_vec, X_test_vec, vectorizer
